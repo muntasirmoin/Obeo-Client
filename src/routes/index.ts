d55c80adeeba76f7b270868ff5aa2c35c0a267e9
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-10-13 19:11:06 +0600

    change import in route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import App from "@/App";
 import GuestBillPaymentPage from "@/Features/guestBillPayment/pages/guestBillPayment";
 import NightAudit from "@/Features/nightAudit/pages/NightAudit";
 import serviceBillPage from "@/Features/serviceBill/pages/serviceBillPage";
+
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
